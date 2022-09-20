5dc7bf2123f8d51e6550bccd7e23efad1d4da65f
Art Tosborvorn <me@artt.dev>
2022-09-20 23:23:52 +0700

    add custom keymap

---
+++ keyboards/sofle/keyhive/keymaps/artt/rules.mk
@@ -0,0 +1,5 @@
+MOUSEKEY_ENABLE = yes       # Mouse keys
+NKRO_ENABLE = yes           # Enable N-Key Rollover
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
+OLED_ENABLE = no
