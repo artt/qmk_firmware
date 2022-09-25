3156bf5a38b4655e3ffc3ff1ef9fe37cd208a3a8
Art Tosborvorn <me@artt.dev>
2022-09-25 17:07:46 +0700

    still missing g_led_

--- keyboards/sofle/keyhive/keymaps/artt/rules.mk
+++ keyboards/sofle/keyhive/keymaps/artt/rules.mk
@@ -1,5 +1,7 @@
 MOUSEKEY_ENABLE = yes       # Mouse keys
 NKRO_ENABLE = yes           # Enable N-Key Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
-RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
+RGBLIGHT_ENABLE = no       # Enable keyboard RGB underglow
+RGB_MATRIX_ENABLE = yes
+RGB_MATRIX_DRIVER = WS2812
 OLED_ENABLE = no
