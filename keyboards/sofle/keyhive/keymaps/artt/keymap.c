d6230f2de4c498b8f9168a32db0cad63c784c5b6
Art Tosborvorn <me@artt.dev>
2022-10-26 20:16:01 +0700

    x

--- keyboards/sofle/keyhive/keymaps/artt/keymap.c
+++ keyboards/sofle/keyhive/keymaps/artt/keymap.c
@@ -170,7 +170,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   QK_GESC,  KC_Q   ,  KC_W   ,  KC_E   ,  KC_R   ,  KC_T   ,  KC_PGDN,       KC_VOLD,  KC_Y   ,  KC_U   ,  KC_I   ,  KC_O   ,  KC_P   ,  KC_BSPC,
   KC_TAB,   KC_A   ,  KC_S   ,  KC_D   ,  KC_F   ,  KC_G   ,  KC_MPLY,       KC_MUTE,  KC_H   ,  KC_J   ,  KC_K   ,  KC_L   ,  KC_SCLN,  KC_QUOT,
   KC_LALT,  KC_Z   ,  KC_X   ,  KC_C   ,  KC_V   ,  KC_B   ,  KC_PGUP,       KC_VOLU,  KC_N   ,  KC_M   ,  KC_COMM,  KC_DOT ,  KC_SLSH,  KC_LCTRL,
-                      XXXXXXX,  XXXXXXX,  KC_LGUI,  LO_ENT ,  SHF_BSPC,      NAV_MIN,  RA_SPC ,  SHF_BSPC, XXXXXXX,  XXXXXXX
+                      XXXXXXX,  XXXXXXX,  KC_LGUI,  LO_ENT ,  SHF_BSPC,      SHF_BSPC, RA_SPC ,  NAV_MIN,  XXXXXXX,  XXXXXXX
 ),
 
 [_LOWER] = LAYOUT_via(
