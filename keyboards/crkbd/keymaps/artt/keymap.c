6c1d1d53675da017c36c60b2c0f3920167c12e9c
artt <me@artt.dev>
2024-07-28 23:45:15 +0700

    x

--- keyboards/crkbd/keymaps/artt/keymap.c
+++ keyboards/crkbd/keymaps/artt/keymap.c
@@ -46,22 +46,22 @@ enum custom_layers {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_BASE] = LAYOUT_split_3x6_3(
-    QK_GESC,  KC_Q   ,  KC_W   ,  KC_E   ,  KC_R   ,  KC_T   ,         KC_Y   ,  KC_U   ,  KC_I   ,  KC_O   ,  KC_P   ,  KC_CAPS,
+    KC_ESC,   KC_Q   ,  KC_W   ,  KC_E   ,  KC_R   ,  KC_T   ,         KC_Y   ,  KC_U   ,  KC_I   ,  KC_O   ,  KC_P   ,  KC_CAPS,
     KC_TAB,   KC_A   ,  KC_S   ,  KC_D   ,  KC_F   ,  KC_G   ,         KC_H   ,  KC_J   ,  KC_K   ,  KC_L   ,  KC_SCLN,  KC_QUOT,
     KC_LALT,  KC_Z   ,  KC_X   ,  KC_C   ,  KC_V   ,  KC_B   ,         KC_N   ,  KC_M   ,  KC_COMM,  KC_DOT ,  KC_SLSH,  KC_LCTRL,
                                   KC_LGUI,  LO_ENT ,  SHF_BSPC,        SHF_BSPC, RA_SPC ,  NAV_MIN
 ),
 
 [_NUM] = LAYOUT_split_3x6_3(
-    KC_GRV,   GUI_LBRK, GUI_RBRK, LBRK,     RBRK,     XXXXXXX,         KC_PSLS,  KC_KP_7,  KC_KP_8,  KC_KP_9,  KC_PAST,  _______,
-    KC_DEL,   KC_LABK,  KC_RABK,  KC_LPRN,  KC_RPRN,  XXXXXXX,         KC_PMNS,  KC_KP_4,  KC_KP_5,  KC_KP_6,  KC_PPLS,  _______,
+    _______,  GUI_LBRK, GUI_RBRK, LBRK,     RBRK,     XXXXXXX,         KC_PSLS,  KC_KP_7,  KC_KP_8,  KC_KP_9,  KC_PAST,  XXXXXXX,
+    KC_DEL,   KC_LABK,  KC_RABK,  KC_LPRN,  KC_RPRN,  XXXXXXX,         KC_PMNS,  KC_KP_4,  KC_KP_5,  KC_KP_6,  KC_PPLS,  XXXXXXX,
     _______,  XXXXXXX,  XXXXXXX,  KC_LCBR,  KC_RCBR,  KC_COMMA,        KC_PDOT,  KC_KP_1,  KC_KP_2,  KC_KP_3,  KC_PEQL,  _______,
                                   _______,  _______,  _______,         _______,  _______,  KC_KP_0
 ),
 
 [_SYM] = LAYOUT_split_3x6_3(
-    KC_TILD,  KC_F1  ,  KC_F2  ,  KC_F3  ,  KC_F4  ,  XXXXXXX,         KC_VOLU,  KC_F9,    KC_F10,   KC_F11,   KC_F12,   _______,
-    KC_DEL,   KC_AT,    KC_ASTR,  KC_DLR,   KC_CIRC,  XXXXXXX,         KC_MUTE,  KC_AMPR,  KC_HASH,  KC_EXLM,  KC_PERC,  _______,
+    KC_GRV,   KC_F1  ,  KC_F2  ,  KC_F3  ,  KC_F4  ,  XXXXXXX,         KC_VOLU,  KC_F9,    KC_F10,   KC_F11,   KC_F12,   XXXXXXX,
+    KC_DEL,   KC_AT,    KC_ASTR,  KC_DLR,   KC_CIRC,  XXXXXXX,         KC_MUTE,  KC_AMPR,  KC_HASH,  KC_EXLM,  KC_PERC,  KC_GRV,
     _______,  KC_F5,    KC_F6,    KC_F7,    KC_F8,    XXXXXXX,         KC_VOLD,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_BSLS,  _______,
                                   _______,  _______,  _______,         _______,  _______,  _______
 ),
@@ -74,8 +74,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 [_NAV] = LAYOUT_split_3x6_3(
-    _______,  XXXXXXX,  KC_WH_L,  KC_MS_U,  KC_WH_R,  KC_WH_U,         XXXXXXX,  KC_HOME,  KC_UP,    KC_END,   KC_PGUP,  _______,
-    _______,  XXXXXXX,  KC_MS_L,  KC_MS_D,  KC_MS_R,  KC_WH_D,         KC_ACL2,  KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_PGDN,  _______,
+    XXXXXXX,  XXXXXXX,  KC_WH_L,  KC_MS_U,  KC_WH_R,  KC_WH_U,         XXXXXXX,  KC_HOME,  KC_UP,    KC_END,   KC_PGUP,  XXXXXXX,
+    XXXXXXX,  XXXXXXX,  KC_MS_L,  KC_MS_D,  KC_MS_R,  KC_WH_D,         KC_ACL2,  KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_PGDN,  XXXXXXX,
     _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,         KC_ACL0,  KC_BTN1,  KC_BTN3,  KC_BTN2,  XXXXXXX,  _______,
                                   _______,  _______,  _______,         _______,  _______,  _______
 ),
@@ -83,6 +83,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 layer_state_t layer_state_set_user(layer_state_t state) {
     return update_tri_layer_state(state, _NUM, _SYM, _ADJUST);
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case KC_MISSION_CONTROL:
+            if (record->event.pressed) {
+                host_consumer_send(0x29F);
+            } else {
+                host_consumer_send(0);
+            }
+            return false;  // Skip all further processing of this key
+        case KC_LAUNCHPAD:
+            if (record->event.pressed) {
+                host_consumer_send(0x2A0);
+            } else {
+                host_consumer_send(0);
+            }
+            return false;  // Skip all further processing of this key
+        default:
+            return true;  // Process all other keycodes normally
+    }
 }
 
 #ifdef OLED_ENABLE
@@ -274,10 +295,10 @@ void rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
                         h = 0; s = 0; v = 255;
                     }
                     else if (kc >= KC_NUM && kc <= KC_PEQL) {    // numpad symbols
-                        h = 0; s = 0; v = 32;
+                        h = 0; s = 0; v = 64;
                     }
                     else if (kc >= KC_F1 && kc <= KC_F12) {    // F1-F12
-                        h = 0; s = 0; v = 32;
+                        h = 0; s = 0; v = 64;
                     }
                     else if (kc == QK_BOOT) {
                         h = 0; s = 255; v = 255;
